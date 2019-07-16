4217dbecb8730028171a5d37fb13450bcc8b64cb
JoanneWang <joanneccwang@gmail.com>
2019-07-17 00:13:16 +0800

    FEATURE: Add huge tomato icon on main screen

--- Week1_Pomodoro/src/components/MainScreen.vue
+++ Week1_Pomodoro/src/components/MainScreen.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="main-screen">
-
+    <div class="clock-container">
+    <icon icon-type="tomato_small_color" :size="300"></icon>
+    </div>
   </div>
 </template>
 <script>
@@ -13,6 +15,14 @@ $background-color: #EAEAEA;
 
 .main-screen {
   background: $background-color;
+  display: flex;
+
+  align-items: center;
+  justify-content: center;
+}
+
+.clock-container {
+
 }
 </style>
 
