9bdae67a5a7db021e7bf811ece2129214e5e78de
JoanneWang <joanneccwang@gmail.com>
2019-07-16 16:16:59 +0800

    FEATURE: Add Icon component and include all icons

---
+++ Week1_Pomodoro/src/components/basic/Icon.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="icon">
+    <div :class="`icon-${iconType}`"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'icon',
+  props: {
+    iconType: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.icon {
+  @mixin IconType($type) {
+    .icon-#{$type} {
+      background: url("../../assets/icons/#{$type}.svg") no-repeat center center;
+      background-size: 20px 20px;
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  display: inline-block;
+  height: 30px;
+  width: 30px;
+
+  @include IconType('tomato_small_color');
+  @include IconType('tomato_small_gray');
+
+  @include IconType('add_red');
+  @include IconType('add_white');
+  @include IconType('analysis_red');
+  @include IconType('analysis_white');
+  @include IconType('arrow');
+  @include IconType('complete');
+  @include IconType('list_red');
+  @include IconType('list_whilte');
+  @include IconType('pause_gray');
+  @include IconType('pause_red');
+  @include IconType('play_gray');
+  @include IconType('play_white');
+  @include IconType('reset_gray');
+  @include IconType('reset_red');
+  @include IconType('ringtone_red');
+  @include IconType('ringtone_white');
+  @include IconType('start_gray');
+  @include IconType('start_red');
+  @include IconType('stop_gray');
+  @include IconType('stop_white');
+}
+
+
+</style>
