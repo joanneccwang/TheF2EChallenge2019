58441153675cd863da4a1706ec62df00d550ac3a
JoanneWang <joanneccwang@gmail.com>
2019-08-09 15:56:16 +0800

    DEPLOY: Update Week1_Pomodoro

--- Week1_Pomodoro/.eslintrc.js
+++ Week1_Pomodoro/.eslintrc.js
@@ -46,6 +46,6 @@ module.exports = {
     }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'warning' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
